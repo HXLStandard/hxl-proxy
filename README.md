5f1029c17253779c6434d9bd66b8a325b298d9cf
David Megginson <david.megginson@megginson.com>
2015-02-09 07:43:28 -0500

    Added license and readme.

---
+++ README.md
@@ -0,0 +1,16 @@
+hxl-proxy
+=========
+
+Python-/flask-based web proxy for transforming a HXL dataset dynamically.
+
+http://hxlstandard.org
+
+# Usage
+
+Install libhxl-python and flask.
+
+```
+python hxl-proxy.py
+```
+
+Typically, the proxy will be running on port 5000 for local use.  For web deployment, see the flask documentation.
--- do-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#https://docs.google.com/spreadsheets/d/1paoIpHiYo7dy_dnf_luUSfowWDwNAWwS3z4GHL2J7Rc/export?format=csv&id=1paoIpHiYo7dy_dnf_luUSfowWDwNAWwS3z4GHL2J7Rc&gid=2077872077
-wget -q -O - 'http://127.0.0.1:5000/filter?format=csv&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1paoIpHiYo7dy_dnf_luUSfowWDwNAWwS3z4GHL2J7Rc%2Fexport%3Fformat%3Dcsv%26id%3D1paoIpHiYo7dy_dnf_luUSfowWDwNAWwS3z4GHL2J7Rc%26gid%3D2077872077'
