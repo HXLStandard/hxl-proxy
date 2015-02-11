1ccaf244e0c3416969d3eb13c6a1cf379e3e0592
David Megginson <david.megginson@megginson.com>
2015-02-10 21:53:10 -0500

    Setup filter properly at the start as well.

--- static/filter-form.js
+++ static/filter-form.js
@@ -1,13 +1,17 @@
 // Hide all hideable fields
 $(".hideable").hide();
 
+function setup_fieldset(node) {
+    filter_name = $(node).find(".field_filter select").val();
+    $(node).find(".hideable").hide();
+    $(node).find(".filter_field_" + filter_name).show();
+}
+
 // Add triggers to hide/show fields based on filter
 $("#filter-form fieldset").each(function (index) {
     var filter_node = this;
+    setup_fieldset(filter_node);
     $(filter_node).find(".field_filter select").on("change", function () {
-        // hide everything
-        var filter_name = $(this).val();
-        $(filter_node).find(".hideable").hide();
-        $(filter_node).find(".filter_field_" + filter_name).show();
+        setup_fieldset(filter_node);
     })
 });
