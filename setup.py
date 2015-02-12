bf7465aabe23cd792f58fa93efcd6841eb34f6e4
David Megginson <david.megginson@megginson.com>
2015-02-11 22:10:51 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from setuptools import setup
+dependency_links=[
+    'git+https://github.com/Toblerity/Shapely.git@maint#egg=Shapely',
+]
+
+setup(
+    name = 'hxl-proxy',
+    packages = ['hxl_proxy'],
+    version = '0.0.1',
+    description = 'Flask-based web proxy for HXL',
+    author='David Megginson',
+    author_email='contact@megginson.com',
+    url='https://github.com/HXLStandard/hxl-proxy',
+    install_requires=['flask', 'libhxl'],
+)
