07cebafa693c509ac2a2b1e624a0aa8d1f3126b8
David Megginson <david.megginson@megginson.com>
2015-02-19 17:58:43 -0500

    Modified map code to use HXL class.

--- hxl_proxy/static/show-map.js
+++ hxl_proxy/static/show-map.js
@@ -7,34 +7,26 @@ map.addLayer(osm);
 
 $.get(csv_url, function(csvString) {
     var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
-    lat_index = hxl_get_tag_index(arrayData, '#lat_deg');
-    lon_index = hxl_get_tag_index(arrayData, '#lon_deg');
-
-    if (lat_index == null || lon_index == null) {
-        alert("Dataset must have #lat_deg and #lon_deg tags for mapping.");
-        return;
-    }
+    var hxl = new HXLDataset(arrayData);
 
     var markers = new L.MarkerClusterGroup();
-    for (i = lat_index[0] + 1; i < arrayData.length; i++) {
-        row = arrayData[i];
-        lat = 0.0 + row[lat_index[1]];
-        lon = 0.0 + row[lon_index[1]];
-        var marker = L.marker([lat, lon]);
-        marker.bindPopup('Location info');
-        markers.addLayer(marker);
-    }
-    map.addLayer(markers);
-    map.fitBounds(markers.getBounds());
-});
-
-function hxl_get_tag_index(data, tag) {
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < data[i].length; j++) {
-            if (data[i][j] == tag) {
-                return [i, j];
-            }
+    var iterator = hxl.iterator();
+    var seen_latlon = false;
+    while (row = iterator.next()) {
+        var lat = row.get('#lat_deg');
+        var lon = row.get('#lon_deg');
+        if (!isNaN(lat) && !isNaN(lon)) {
+            seen_latlon = true;
+            var marker = L.marker([lat, lon]);
+            console.log(marker);
+            marker.bindPopup('Location info');
+            markers.addLayer(marker);
         }
     }
-    return null;
-}
+    if (seen_latlon) {
+        map.addLayer(markers);
+        map.fitBounds(markers.getBounds());
+    } else {
+        alert("No #lat_deg and #lon_deg values to map.");
+    }
+});
