6b4ac14fc8196878408a76942c6f06b63391576e
David Megginson <david.megginson@megginson.com>
2015-02-19 19:58:42 -0500

    In progress for adding layer control. Not sure how it will work with clustering.

--- hxl_proxy/static/show-map.js
+++ hxl_proxy/static/show-map.js
@@ -1,9 +1,19 @@
 var map = L.map('map-div');
+var map_markers = new L.MarkerClusterGroup();
 
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
 map.addLayer(osm);
+
+var map_layers = {};
+
+function get_map_layer(name) {
+    if (!map_layers[name]) {
+        map_layers[name] = L.layerGroup();
+    }
+    return map_layers[name];
+}
 
 $.get(csv_url, function(csvString) {
     var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
@@ -11,21 +21,28 @@ $.get(csv_url, function(csvString) {
 
     var iterator = hxl.iterator();
     var seen_latlon = false;
-    var markers =  new L.MarkerClusterGroup();
 
     while (row = iterator.next()) {
+        var layer = get_map_layer(row.get(map_layer_tag));
+        console.log(layer);
         var lat = row.get('#lat_deg');
         var lon = row.get('#lon_deg');
         if (!isNaN(lat) && !isNaN(lon)) {
             var marker = L.marker([lat, lon]);
             marker.bindPopup('Location info');
-            markers.addLayer(marker);
+            layer.addLayer(marker);
             seen_latlon = true;
         }
     }
     if (seen_latlon) {
-        map.addLayer(markers);
-        map.fitBounds(markers.getBounds());
+        var layers = [];
+        for (name in map_layers) {
+            map_markers.addLayer(map_layers[name]);
+            layers.push(map_layers[name]);
+        }
+        map.addLayer(map_markers);
+        L.control.layers(null, map_layers).addTo(map);
+        map.fitBounds(map_markers.getBounds());
     } else {
         alert("No #lat_deg and #lon_deg values to map.");
     }
