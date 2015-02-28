8af70b5d5873ff7f7b37e0fbf46fff81ff471bcc
David Megginson <david.megginson@megginson.com>
2015-02-28 18:24:16 -0500

    Modal dialogs are good, but forms validation not working.

--- hxl_proxy/static/hxl-theme/filter-form.js
+++ hxl_proxy/static/hxl-theme/filter-form.js
@@ -3,9 +3,12 @@ $(".hideable").hide();
 
 var field_types = "input,select";
 
-function setup_fieldset(node) {
+function setup_fieldset(node, index) {
     filter_name = $(node).find(".field_filter select").val();
     filter_class = ".fields-" + filter_name;
+    filter_title = "Filter #" + (index + 1) + ": " + (filter_name ? filter_name : '(not set)');
+    $(node).find(".modal-title").text(filter_title);
+    $(node).find(".filter-button").text(filter_title);
     $(node).find(".hideable").hide();
     $(node).find(".hideable").find(field_types).prop("disabled", true);
     $(node).find(filter_class).show();
@@ -13,10 +16,11 @@ function setup_fieldset(node) {
 }
 
 // Add triggers to hide/show fields based on filter
-$("#filter-form fieldset").each(function (index) {
+$("#filter-form div.filter").each(function (index) {
+    console.log(index);
     var filter_node = this;
-    setup_fieldset(filter_node);
+    setup_fieldset(filter_node, index);
     $(filter_node).find(".field_filter select").on("change", function () {
-        setup_fieldset(filter_node);
+        setup_fieldset(filter_node, index);
     })
 });
