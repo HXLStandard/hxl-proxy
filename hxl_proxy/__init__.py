7e801506700813fb1d639099112af1fb02ac751a
David Megginson <david.megginson@megginson.com>
2015-03-21 12:47:54 -0400

    Accept Google Sheets URL without a gid. Fixes #32

--- hxl_proxy/__init__.py
+++ hxl_proxy/__init__.py
@@ -37,11 +37,15 @@ profiles = ProfileManager(app.config['PROFILE_FILE'])
 # Utilities
 #
 
+# https://docs.google.com/spreadsheets/d/1ytPD-f4a8CbNKTfMS3EqZOpBo9LWCk_NDKxJCgmpXA8/edit?usp=sharing
 def munge_url(url):
     """If a URL points to a tab in a Google Sheet, grab the CSV export."""
-    result = re.match(r'https?://docs.google.com.*/spreadsheets/.*([0-9A-Za-z_-]{44}).*gid=([0-9]+)', str(url))
+    result = re.match(r'https?://docs.google.com.*/spreadsheets/.*([0-9A-Za-z_-]{44}).*(?:gid=([0-9]+))?', str(url))
     if result:
-        url = 'https://docs.google.com/spreadsheets/d/{0}/export?format=csv&gid={1}'.format(result.group(1), result.group(2))
+        if result.group(2):
+            url = 'https://docs.google.com/spreadsheets/d/{0}/export?format=csv&gid={1}'.format(result.group(1), result.group(2))
+        else:
+            url = 'https://docs.google.com/spreadsheets/d/{0}/export?format=csv'.format(result.group(1))
     return url
 
 def decode_string(s):
