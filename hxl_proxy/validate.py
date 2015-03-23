7221a1b314a805513d3c42c5957393f3a55df717
David Megginson <david.megginson@megginson.com>
2015-03-22 20:21:19 -0400

    Validation support now works with a custom schema.  Error reports link to specific rows in the data.

--- hxl_proxy/validate.py
+++ hxl_proxy/validate.py
@@ -2,7 +2,7 @@
 Validation support
 """
 
-from hxl.io import URLInput
+from hxl.io import URLInput, HXLReader
 from hxl.schema import readSchema
 from hxl_proxy import munge_url
 
@@ -12,6 +12,7 @@ def do_validate(source, schema_url=None):
     def callback(error):
         errors.append(error)
     schema = make_schema(schema_url)
+    schema.callback = callback
     result = schema.validate(source)
     return errors
 
@@ -19,7 +20,7 @@ def make_schema(schema_url=None):
     """Get a schema object for validation."""
     if schema_url:
         # return a custom schema
-        return readSchema(URLInput(munge_url(schema_url)))
+        return readSchema(HXLReader(URLInput(munge_url(schema_url))))
     else:
         # return the built-in default schema
         return readSchema()
