d35aaaffdc36e1df4c2f7d4bbdf7c3976d092758
David Megginson <david.megginson@megginson.com>
2015-03-29 22:00:21 -0400

    Add comments to tests.

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -23,6 +23,7 @@ else:
 import hxl_proxy
 from hxl_proxy.profiles import ProfileManager, Profile
 
+
 class TestEditPage(unittest.TestCase):
     """Test /data/edit and /data/<key>/edit"""
 
@@ -36,6 +37,7 @@ class TestEditPage(unittest.TestCase):
         rv = self.client.get('/data/edit')
         self.assertTrue('New HXL view' in rv.data)
         self.assertTrue('No data yet' in rv.data)
+
 
 class TestDataPage(unittest.TestCase):
     """Test /data and /data/<key>"""
@@ -56,15 +58,28 @@ class TestDataPage(unittest.TestCase):
 
     @patch(URLOPEN_PATCH)
     def test_url(self, mock):
-        mock.return_value = open(resolve_path('files/basic-dataset.csv'))
+        mock_basic_dataset(mock)
         response = self.client.get('/data?url=http://example.org')
-        self.assertEqual(200, response.status_code)
         self.assertTrue('<h1>New filter preview</h1>' in response.data)
+        self._assert_basic_dataset(response)
+
+    @patch(URLOPEN_PATCH)
+    def test_key(self, mock):
+        mock_basic_dataset(mock)
+        mock.return_value = open(resolve_path('files/basic-dataset.csv'))
+        response = self.client.get('/data/{}'.format(self.key))
+        self.assertTrue('<h1>Sample dataset</h1>' in response.data)
+        self._assert_basic_dataset(response)
+
+    def _assert_basic_dataset(self, response):
+        """Check that we're looking at the basic dataset"""
+        self.assertEqual(200, response.status_code)
         self.assertTrue('Country' in response.data, "header from dataset on page")
         self.assertTrue('#country' in response.data, "hashtag from dataset on page")
         self.assertTrue('Org A' in response.data, "org from dataset on page")
         self.assertTrue(u'Education' in response.data, "sector from dataset on page")
         self.assertTrue('Myanmar' in response.data, "country from dataset on page")
+
 
 class TestValidationPage(unittest.TestCase):
     """Test /data/validate and /data/key/validate"""
@@ -88,13 +103,20 @@ def start_tests(tests):
     with tempfile.NamedTemporaryFile(delete=True) as file:
         tests.filename = file.name
     hxl_proxy.app.config['PROFILE_FILE'] = tests.filename
-    tests.key = ProfileManager(tests.filename).add_profile(Profile({
-        'url': 'http://example.org'
-    }))
+    tests.key = ProfileManager(tests.filename).add_profile(make_profile())
     tests.client = hxl_proxy.app.test_client()
 
 def end_tests(tests):
     """Remove the temporary profile database"""
     os.remove(tests.filename)
-    
 
+def make_profile():
+    profile = Profile({
+        'url': 'http://example.org'
+    })
+    profile.name = 'Sample dataset'
+    return profile
+
+def mock_basic_dataset(mock):
+    mock.return_value = open(resolve_path('files/basic-dataset.csv'))
+        
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -33,7 +33,6 @@ DATA = [
 class TestSetupFilters(unittest.TestCase):
 
     def test_filters(self):
-        """Test the normal case of a filter pipeline."""
         args = {
             'url': 'http://example.org/data.csv',
             'filter_count': 3,
@@ -48,19 +47,17 @@ class TestSetupFilters(unittest.TestCase):
         source = setup_filters(profile)
 
         # check the whole pipeline
-        self.assertEqual('SortFilter', source.__class__.__name__)
-        self.assertEqual('SelectFilter', source.source.__class__.__name__)
-        self.assertEqual('CountFilter', source.source.source.__class__.__name__)
-        self.assertEqual('HXLReader', source.source.source.source.__class__.__name__)
+        self.assertEqual('SortFilter', source.__class__.__name__, "sort filter is fourth")
+        self.assertEqual('SelectFilter', source.source.__class__.__name__, "select filter is third")
+        self.assertEqual('CountFilter', source.source.source.__class__.__name__, "count filter is second")
+        self.assertEqual('HXLReader', source.source.source.source.__class__.__name__, "reader is first")
 
     def test_null_profile(self):
-        """Test passing a null profile to setup_filters"""
-        self.assertIsNone(setup_filters(None))
+        self.assertIsNone(setup_filters(None), "ok to pass None to setup_filters")
 
     def test_null_url(self):
-        "Test passing a profile with no url."
         profile = Profile({})
-        self.assertIsNone(setup_filters(profile))
+        self.assertIsNone(setup_filters(profile), "ok to pass null URL to setup_filters")
 
 
 class TestPipelineFunctions(unittest.TestCase):
@@ -70,7 +67,6 @@ class TestPipelineFunctions(unittest.TestCase):
         self.source = HXLReader(self.input)
 
     def test_add_add_filter(self):
-        """Test constructing a hxl.filters.AddFilter from HTTP parameters."""
         args = {
             'add-tag05': '#country',
             'add-header05': 'Country name',
@@ -78,14 +74,13 @@ class TestPipelineFunctions(unittest.TestCase):
             'add-before05': 'on'
         }
         filter = add_add_filter(self.source, args, 5)
-        self.assertEqual('AddFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
-        self.assertEqual(args['add-tag05'], filter.values[0][0].tag)
-        self.assertEqual(args['add-value05'], filter.values[0][1])
-        self.assertTrue(filter.before)
+        self.assertEqual('AddFilter', filter.__class__.__name__, "add filter from args")
+        self.assertEqual(self.source, filter.source, "source OK")
+        self.assertEqual(args['add-tag05'], filter.values[0][0].tag, "tag OK")
+        self.assertEqual(args['add-value05'], filter.values[0][1], "value OK")
+        self.assertTrue(filter.before, "before ok")
 
     def test_add_clean_filter(self):
-        """Test constructing a hxl.filters.CleanFilter from HTTP parameters."""
         args = {
             'clean-whitespace-tags07': 'adm1,sector-cluster',
             'clean-upper-tags07': 'adm1_id,sector_id',
@@ -94,41 +89,38 @@ class TestPipelineFunctions(unittest.TestCase):
             'clean-number-tags07': 'aff_num+idp,targeted_num'
         }
         filter = add_clean_filter(self.source, args, 7)
-        self.assertEqual('CleanFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
-        self.assertEqual(['#adm1', '#sector-cluster'], [str(p) for p in filter.whitespace])
-        self.assertEqual(['#adm1_id', '#sector_id'], [str(p) for p in filter.upper])
-        self.assertEqual(['#org_id', '#agesex_id'], [str(p) for p in filter.lower])
-        self.assertEqual(['#from_date', '#to_date'], [str(p) for p in filter.date])
-        self.assertEqual(['#aff_num+idp', '#targeted_num'], [str(p) for p in filter.number])
+        self.assertEqual('CleanFilter', filter.__class__.__name__, "clean filter from args")
+        self.assertEqual(self.source, filter.source, "source ok")
+        self.assertEqual(['#adm1', '#sector-cluster'], [str(p) for p in filter.whitespace], "whitespace ok")
+        self.assertEqual(['#adm1_id', '#sector_id'], [str(p) for p in filter.upper], "upper ok")
+        self.assertEqual(['#org_id', '#agesex_id'], [str(p) for p in filter.lower], "lower ok")
+        self.assertEqual(['#from_date', '#to_date'], [str(p) for p in filter.date], "date ok")
+        self.assertEqual(['#aff_num+idp', '#targeted_num'], [str(p) for p in filter.number], "number ok")
 
     def test_add_count_filter(self):
-        """Test constructing a hxl.filters.CountFilter from HTTP parameters."""
         args = {
             'count-tags03': 'country,adm1,adm2+ocha',
             'count-aggregate-tag03': 'targeted_num+f'
         }
         filter = add_count_filter(self.source, args, 3)
-        self.assertEqual('CountFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
-        self.assertEqual(['#country', '#adm1', '#adm2+ocha'], [str(p) for p in filter.count_tags])
-        self.assertEqual('#targeted_num+f', str(filter.aggregate_tag))
+        self.assertEqual('CountFilter', filter.__class__.__name__, "count filter from args")
+        self.assertEqual(self.source, filter.source, "source ok")
+        self.assertEqual(['#country', '#adm1', '#adm2+ocha'], [str(p) for p in filter.count_tags], "tags ok")
+        self.assertEqual('#targeted_num+f', str(filter.aggregate_tag), "aggregate tag ok")
     
     def test_add_cut_filter(self):
-        """Test constructing a hxl.filters.CutFilter from HTTP parameters."""
         args = {
             'cut-include-tags01': 'org,adm1,adm2+pcode',
             'cut-exclude-tags01': 'email+external,name-ngo'
         }
         filter = add_cut_filter(self.source, args, 1)
-        self.assertEqual('CutFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
-        self.assertEqual(['#org', '#adm1', '#adm2+pcode'], [str(p) for p in filter.include_tags])
-        self.assertEqual(['#email+external', '#name-ngo'], [str(p) for p in filter.exclude_tags])
+        self.assertEqual('CutFilter', filter.__class__.__name__, "cut filter from args")
+        self.assertEqual(self.source, filter.source, "source ok")
+        self.assertEqual(['#org', '#adm1', '#adm2+pcode'], [str(p) for p in filter.include_tags], "include ok")
+        self.assertEqual(['#email+external', '#name-ngo'], [str(p) for p in filter.exclude_tags], "exclude ok")
 
     @patch(URLOPEN_PATCH)
     def test_add_merge_filter(self, urlopen_mock):
-        """Test constructing a hxl.filters.MergeFilter from HTTP parameters."""
         urlopen_mock.return_value = 'x'
         args = {
             'merge-keys11': 'adm2_id+pcode',
@@ -138,32 +130,30 @@ class TestPipelineFunctions(unittest.TestCase):
             'merge-url11': 'http://example.org/data.csv'
         }
         filter = add_merge_filter(self.source, args, 11)
-        self.assertEqual('MergeFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
-        self.assertEqual(['#adm2_id+pcode'], [str(p) for p in filter.keys])
-        self.assertEqual(['#lat_deg', '#lon_deg'], [str(p) for p in filter.merge_tags])
-        self.assertTrue(filter.replace)
-        self.assertTrue(filter.overwrite)
+        self.assertEqual('MergeFilter', filter.__class__.__name__, "merge filter from args")
+        self.assertEqual(self.source, filter.source, "source ok")
+        self.assertEqual(['#adm2_id+pcode'], [str(p) for p in filter.keys], "keys ok")
+        self.assertEqual(['#lat_deg', '#lon_deg'], [str(p) for p in filter.merge_tags], "merge tags ok")
+        self.assertTrue(filter.replace, "replace flag ok")
+        self.assertTrue(filter.overwrite, "overwrite flag ok")
         #self.assertEquals(args['merge-url11'], filter.merge_source._input) # need to be able to get URL
 
     def test_add_rename_filter(self):
-        """Test constructing a hxl.filters.RenameFilter from HTTP parameters."""
         args = {
             'rename-oldtag08': 'loc-sensitive',
             'rename-newtag08': 'adm1',
             'rename-header08': 'Provincia'
         }
         filter = add_rename_filter(self.source, args, 8)
-        self.assertEqual('RenameFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
+        self.assertEqual('RenameFilter', filter.__class__.__name__, "rename filter from args")
+        self.assertEqual(self.source, filter.source, "source ok")
         for spec in filter.rename:
             # XXX assuming just one key, or else this will break badly
-            self.assertEqual('#loc-sensitive', str(spec[0]))
-            self.assertEqual('#adm1', spec[1].display_tag)
-            self.assertEqual('Provincia', spec[1].header)
+            self.assertEqual('#loc-sensitive', str(spec[0]), "original tag pattern ok")
+            self.assertEqual('#adm1', spec[1].display_tag, "replacement tag pattern ok")
+            self.assertEqual('Provincia', spec[1].header, "header ok")
 
     def test_add_select_filter(self):
-        """Test constructing a hxl.filters.SelectFilter from HTTP parameters."""
         args = {
             'select-query09-01': 'sector+cluster=WASH',
             'select-query09-02': 'aff_num-adult<3',
@@ -171,22 +161,37 @@ class TestPipelineFunctions(unittest.TestCase):
             'select-reverse09': 'on'
         }
         filter = add_select_filter(self.source, args, 9)
-        self.assertEqual('SelectFilter', filter.__class__.__name__)
-        self.assertEqual(self.source, filter.source)
-        self.assertEqual(['#sector+cluster', '#aff_num-adult', '#org'], [str(q.pattern) for q in filter.queries])
-        self.assertEqual([operator.eq, operator.lt, operator.ne], [q.op for q in filter.queries])
-        self.assertEqual(['WASH', '3', 'UNICEF'], [str(q.value) for q in filter.queries])
-        self.assertTrue(filter.reverse)
+        self.assertEqual('SelectFilter', filter.__class__.__name__, "select filter from args")
+        self.assertEqual(self.source, filter.source, "source ok")
+        self.assertEqual(
+            ['#sector+cluster', '#aff_num-adult', '#org'],
+            [str(q.pattern) for q in filter.queries],
+            "tag patterns ok"
+        )
+        self.assertEqual(
+            [operator.eq, operator.lt, operator.ne],
+            [q.op for q in filter.queries],
+            "operators ok"
+        )
+        self.assertEqual(
+            ['WASH', '3', 'UNICEF'],
+            [str(q.value) for q in filter.queries],
+            "values ok"
+        )
+        self.assertTrue(filter.reverse, "reverse flag ok")
 
     def test_add_sort_filter(self):
-        """Test constructing a hxl.filters.SortFilter from HTTP parameters."""
         args = {
             'sort-tags13': 'country,adm1,org+ngo',
             'sort-reverse13': 'on'
         }
         filter = add_sort_filter(self.source, args, 13)
-        self.assertEqual('SortFilter', filter.__class__.__name__)
-        self.assertEqual(['#country', '#adm1', '#org+ngo'], [str(p) for p in filter.sort_tags])
-        self.assertTrue(filter.reverse)
+        self.assertEqual('SortFilter', filter.__class__.__name__, "sort filter from args")
+        self.assertEqual(
+            ['#country', '#adm1', '#org+ngo'],
+            [str(p) for p in filter.sort_tags],
+            "tags ok"
+        )
+        self.assertTrue(filter.reverse, "reverse flag ok")
 
 # end
