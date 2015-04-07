96b74864412c2004d9dbf8687c099cee6f90e4b2
David Megginson <david.megginson@megginson.com>
2015-04-07 15:58:52 -0400

    Fancy titles.

--- hxl_proxy/analysis.py
+++ hxl_proxy/analysis.py
@@ -1,6 +1,10 @@
+import operator
+import urllib
+
 from hxl.model import TagPattern
 from hxl.io import HXLReader
 from hxl.filters.cache import CacheFilter
+from hxl.filters.select import SelectFilter, Query
 
 from hxl_proxy.util import norm, make_input
 
@@ -27,15 +31,8 @@ class Analysis:
 
     def __init__(self, args):
         self.args = args
-        self.filter_tags = [] # TODO
         self._saved_source = None
-
-    @property
-    def source(self):
-        """Open the input on initial request."""
-        if not self._saved_source:
-            self._saved_source = CacheFilter(HXLReader(make_input(self.args.get('url'))))
-        return self._saved_source
+        self._saved_filters = None
 
     def get_value_counts(self, pattern):
         """Get a list of values and frequencies for a tag pattern."""
@@ -48,22 +45,107 @@ class Analysis:
                 if value:
                     total += 1
                     if occurs.get(key):
-                        occurs[key] += 1
+                        occurs[key]['count'] += 1
                     else:
-                        occurs[key] = 1
-        return [ { 'value': key, 'count': occurs[key], 'percentage': float(occurs[key]) / total } for key in occurs ]
+                        occurs[key] = { 'count': 1, 'orig': value }
+        return [ { 'value': key, 'orig': occurs[key]['orig'], 'count': occurs[key]['count'], 'percentage': float(occurs[key]['count']) / total } for key in occurs ]
 
     def get_top_values(self, pattern):
         return sorted(self.get_value_counts(pattern), key = lambda entry: entry['count'], reverse=True)
+
+    def make_query(self, pattern=None, value=None):
+        """Construct a query string"""
+        queries = [ [ urllib.quote(str(filter['pattern'])[1:]), urllib.quote(filter['value']) ] for filter in self.filters]
+        if pattern:
+            queries.append([ urllib.quote(str(pattern)[1:]), urllib.quote(value) ])
+        queries.append([ urllib.quote('url'), urllib.quote(self.args.get('url')) ])
+        return '&'.join(map(lambda item: '='.join(item), queries))
+
+    @property
+    def title(self, tag_pattern=None):
+        if tag_pattern:
+            return "List of " + str(tag_pattern)
+        else:
+            who = self.who
+            what = self.what
+            where = self.where
+            if who and what and where:
+                return 'What is {} doing for {} in {}?'.format(who, what, where)
+            elif who and what:
+                return 'Where is {} working in {}?'.format(who, what)
+            elif who and where:
+                return 'What is {} doing in {}?'.format(who, where)
+            elif what and where:
+                return 'Who is working on {} in {}?'.format(what, where)
+            elif who:
+                return 'What is {} working on, where?'.format(who)
+            elif what:
+                return 'Who is working on {}, where?'.format(what)
+            elif where:
+                return 'Who is doing what in {}?'.format(where)
+            else:
+                return 'Who is doing what, where?'
+
+    @property
+    def who(self):
+        for filter in self.filters:
+            if filter['pattern'].tag == '#org':
+                return filter['value']
+        return None
+
+    @property
+    def what(self):
+        for filter in self.filters:
+            if filter['pattern'].tag in ('#sector', '#subsector'):
+                return filter['value']
+        return None
+
+    @property
+    def where(self):
+        for filter in self.filters:
+            if filter['pattern'].tag in ('#region', '#country', '#adm1', '#adm2', '#adm3', '#adm4', '#adm5', '#loc'):
+                return filter['value']
+        return None
+
+    def overview_url(self, pattern=None, value=None):
+        return '/analysis?{}'.format(self.make_query(pattern, value))
+
+    def tag_url(self, tag_pattern, pattern=None, value=None):
+        return '/analysis/{}?{}'.format(urllib.quote(str(tag_pattern)[1:]), self.make_query(pattern, value))
 
     @property
     def patterns(self):
         patterns = []
         for tag_list in self.TAGS:
-            print(tag_list)
             for tag_info in tag_list:
-                if tag_info[0] not in self.filter_tags:
+                pattern = TagPattern.parse(tag_info[0])
+                if (pattern.tag not in [filter['pattern'].tag for filter in self.filters]) and (pattern.tag in [column.tag for column in self.source.columns]):
                     patterns.append(tag_info)
                     break
         return patterns
+
+    @property
+    def filters(self):
+        if self._saved_filters is None:
+            filters = []
+            for pattern in self.args.to_dict():
+                if pattern == 'url':
+                    continue
+                filters.append({
+                    'pattern': TagPattern.parse(pattern),
+                    'value': self.args.get(pattern).encode('utf8')
+                })
+            self._saved_filters = filters
+        return self._saved_filters
+
+    @property
+    def source(self):
+        """Open the input on initial request."""
+        if not self._saved_source:
+            source = HXLReader(make_input(self.args.get('url')))
+            for filter_data in self.filters:
+                query = Query(filter_data['pattern'], operator.eq, filter_data['value'])
+                source = SelectFilter(source, queries=[query])
+            self._saved_source = CacheFilter(source)
+        return self._saved_source
 
