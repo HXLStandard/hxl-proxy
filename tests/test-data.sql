11f03dc4eba2c208af61f0f5662e7c17635461ba
David Megginson <david.megginson@megginson.com>
2016-02-22 17:03:09 -0500

    Added schema and test data.

---
+++ tests/test-data.sql
@@ -0,0 +1,9 @@
+insert into Users (user_id, email, name, name_given, name_family, last_login)
+values
+('user1', 'user1@example.org', 'User One', 'User', 'One', '2015-11-11 11:00:00'),
+('user2', 'user2@example.org', 'User Two', 'User', 'Two', '2016-11-11 11:00:00');
+
+insert into Recipes (recipe_id, user_id, name, description, cloneable, stub, args, date_created, date_modified)
+values
+('AAAAA', 'user1', 'Recipe #1', 'First test recipe', 1, 'recipe1', '{}', '2015-11-11 11:00:00', '2015-11-11 11:00:00'),
+('BBBBB', 'user2', 'Recipe #2', null, 0, null, '{}', '2016-11-11 11:00:00', '2016-11-11 11:00:00');
