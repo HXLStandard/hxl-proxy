8e7a909548d39c5693f53df7a4aeaa95180d69db
David Megginson <david.megginson@megginson.com>
2018-04-16 10:27:11 -0400

    Update schema again.
    - Go back to dropping existing tables. It will never make sense to run
      this against a populated database (will need to use SQL-encoded
      change scripts for table changes).
    - Use UTF-8 as the default encoding.

--- hxl_proxy/schema-mysql.sql
+++ hxl_proxy/schema-mysql.sql
@@ -1,14 +1,22 @@
-
-create table if not exists Users (
+/**
+ * Schema to empty and recreate the HXL Proxy database in MySQL.
+ *
+ * Warning: will delete any existing data.
+ * Use this schema only for a fresh installation.
+ */
+ 
+drop table if exists Users;
+create table Users (
        user_id varchar(128) primary key,
        email varchar(128) not null,
        name varchar(128) not null,
        name_given varchar(64),
        name_family varchar(64),
        last_login datetime not null
-);
+) default charset=utf8;
 
-create table if not exists Recipes (
+drop table if exists Recipes;
+create table Recipes (
        recipe_id char(6) primary key,
        name varchar(128) not null,
        passhash char(32) not null,
@@ -18,4 +26,4 @@ create table if not exists Recipes (
        args text not null,
        date_created datetime not null,
        date_modified datetime not null
-);
+) default charset=utf8;
