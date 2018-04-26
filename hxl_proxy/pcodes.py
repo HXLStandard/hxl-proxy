f6f37ece55554d0f964c184e558a366cc4d3302b
David Megginson <david.megginson@megginson.com>
2018-04-26 13:07:52 -0400

    Updated doc comments for #214 and moved code out of with block.

--- hxl_proxy/pcodes.py
+++ hxl_proxy/pcodes.py
@@ -1,4 +1,12 @@
-"""Special data sources"""
+"""
+Services for retrieving P-code lists from iTOS
+David Megginson
+Started April 2018
+
+License: Public Domain
+Documentation: https://github.com/HXLStandard/hxl-proxy/wiki
+"""
+
 import csv, logging, re, requests, sys, werkzeug
 
 logger = logging.getLogger(__name__)
@@ -6,11 +14,11 @@ logger = logging.getLogger(__name__)
 #
 # Constants
 #
-
 COUNTRY_URL_PATTERN = 'http://gistmaps.itos.uga.edu/arcgis/rest/services/COD_External/{country}_pcode/MapServer/layers?f=pjson'
+"""Pattern for constructing an iTOS URL for country metadata"""
 
 PCODES_URL_PATTERN = 'http://gistmaps.itos.uga.edu/arcgis/rest/services/COD_External/{country}_pcode/MapServer/{level}/query?where=1%3D1&outFields=*&returnGeometry=false&f=pjson'
-"""Pattern for constructing an iTOS query URL."""
+"""Pattern for constructing an iTOS URL for P-codes"""
 
 PCODE_HEADER_PATTERNS = {
     r'^admin0RefName$': '#country+name+ref',
@@ -34,19 +42,21 @@ def get_country_levels(country):
     levels = {}
     country = country.upper()
     url = COUNTRY_URL_PATTERN.format(country=country)
+
     with requests.get(url) as result:
         data = result.json()
-        if 'error' in data:
-            raise werkzeug.exceptions.NotFound("iTOS P-code service does not support country: {}".format(country))
-        for layer in result.json().get('layers'):
-            result = re.match(r'^Admin(\d)$', layer['name'])
-            if result:
-                id = layer['id']
-                l = result.group(1)
-                if result == 0:
-                    levels['country'] = id
-                else:
-                    levels['adm{}'.format(l)] = id
+
+    if 'error' in data:
+        raise werkzeug.exceptions.NotFound("iTOS P-code service does not support country: {}".format(country))
+    for layer in result.json().get('layers'):
+        result = re.match(r'^Admin(\d)$', layer['name'])
+        if result:
+            id = layer['id']
+            l = result.group(1)
+            if result == 0:
+                levels['country'] = id
+            else:
+                levels['adm{}'.format(l)] = id
     return levels
 
 
@@ -64,42 +74,41 @@ def extract_pcodes(country, level, fp):
     if not level in country_levels:
         raise werkzeug.exceptions.NotFound("iTOS P-code service does support admin level {} for {}".format(level, country))
 
+    # Read the data from iTOS
     url = PCODES_URL_PATTERN.format(country=country, level=country_levels[level])
-
     with requests.get(url) as result:
-        output = csv.writer(fp)
         data = result.json()
+    if "error" in data:
+        raise werkzeug.exceptions.BadGateway('Unexpected iTOS P-code service error (try again)')
 
-        if "error" in data:
-            raise werkzeug.exceptions.BadGateway('Unexpected iTOS P-code service error (try again)')
+    # Set up the header and hashtag rows
+    headers = []
+    hashtags = []
+    for field in data['fields']:
+        header = field['name']
+        for pattern in PCODE_HEADER_PATTERNS:
+            if re.fullmatch(pattern, header):
+                hashtag = re.sub(pattern, PCODE_HEADER_PATTERNS[pattern], header)
+                headers.append(header)
+                hashtags.append(hashtag)
+                break
 
-        # Set up the header and hashtag rows
-        headers = []
-        hashtags = []
-        for field in data['fields']:
-            header = field['name']
-            for pattern in PCODE_HEADER_PATTERNS:
-                if re.fullmatch(pattern, header):
-                    hashtag = re.sub(pattern, PCODE_HEADER_PATTERNS[pattern], header)
-                    headers.append(header)
-                    hashtags.append(hashtag)
-                    break
+    # If we didn't get any columns, it's bad data
+    if len(hashtags) == 0:
+        raise werkzeug.exceptions.BadGateway('Unexpected iTOS P-code service error (try again)')
 
-        # If we didn't get any columns, it's bad data
-        if len(hashtags) == 0:
-            raise werkzeug.exceptions.NotFound('no P-codes available for this admin level')
+    # Generate the CSV headers and hashtags
+    output = csv.writer(fp)
+    output.writerow(headers)
+    output.writerow(hashtags)
 
-        # Print the headers and hashtags
-        output.writerow(headers)
-        output.writerow(hashtags)
-
-        # Process the rows
-        for entry in data['features']:
-            row = []
-            for header in headers:
-                # only fields in the header row get included
-                if header in entry['attributes']:
-                    row.append(entry['attributes'][header])
-            output.writerow(row)
+    # Generate the CSV data rows
+    for entry in data['features']:
+        row = []
+        for header in headers:
+            # only fields in the header row get included
+            if header in entry['attributes']:
+                row.append(entry['attributes'][header])
+        output.writerow(row)
 
 # end
