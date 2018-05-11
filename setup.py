484cc2742877c060c76a78e520b8b46698542b25
David Megginson <david.megginson@megginson.com>
2018-05-11 10:39:58 -0400

    Require libhxl==4.7.1 for hotfix HXLStandard/libhxl-python#154

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     url='https://github.com/HXLStandard/hxl-proxy',
     include_package_data = True,
     zip_safe = False,
-    install_requires=['flask-cache>=0.13', 'libhxl==4.7', 'ckanapi>=3.5', 'flask==0.12.4', 'requests_cache', 'mysql-connector-python'],
+    install_requires=['flask-cache>=0.13', 'libhxl==4.7.1', 'ckanapi>=3.5', 'flask==0.12.4', 'requests_cache', 'mysql-connector-python'],
     test_suite = "tests",
     tests_require = ['mock']
 )
