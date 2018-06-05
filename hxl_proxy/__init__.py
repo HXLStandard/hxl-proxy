5afa660241f42d0c15bc80440ad9d104fff0ca7f
David Megginson <david.megginson@megginson.com>
2018-06-05 19:38:25 -0400

    Require libhxl==4.8.3 for critical fixes and features Prepare 1.8.2 release

--- hxl_proxy/__init__.py
+++ hxl_proxy/__init__.py
@@ -16,7 +16,7 @@ from flask_cache import Cache
 import werkzeug.datastructures
 from . import reverse_proxied
 
-__version__="1.8.1"
+__version__="1.8.2"
 """Module version number
 See https://www.python.org/dev/peps/pep-0396/
 """
