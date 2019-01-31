c279bbf82b0ea778e1dbe2bfac09c21bc3e199f8
David Megginson <david.megginson@gmail.com>
2019-01-31 11:05:33 -0500

    Prepare for 1.13 release.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     name = 'hxl-proxy',
     packages = ['hxl_proxy'],
     package_data={'hxl_proxy': ['*.sql']},
-    version = "1.12",
+    version = "1.13",
     description = 'Flask-based web proxy for HXL',
     author='David Megginson',
     author_email='contact@megginson.com',
@@ -22,7 +22,7 @@ setuptools.setup(
     zip_safe = False,
     install_requires=[
         'requests>=2.11',
-        'libhxl==4.12',
+        'libhxl==4.13',
         'ckanapi>=3.5',
         'flask==0.12.4',
         'flask-cache>=0.13',
