72e78bd80719f895c192f041f0e0608330d0317e
David Megginson <david.megginson@gmail.com>
2019-03-12 11:27:42 -0400

    Prepare 1.14.1 interim release #262

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     name = 'hxl-proxy',
     packages = ['hxl_proxy'],
     package_data={'hxl_proxy': ['*.sql']},
-    version = "1.14",
+    version = "1.14.1",
     description = 'Flask-based web proxy for HXL',
     author='David Megginson',
     author_email='contact@megginson.com',
