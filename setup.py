3f1ab7c5e2d6f093255d101302a19d28b30fbf63
David Megginson <david.megginson@gmail.com>
2020-04-23 18:28:52 -0400

    Start preparing 1.19 release #312

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     zip_safe = False,
     install_requires=[
         'requests>=2.11',
-        'libhxl==4.18',
+        'libhxl==4.19',
         'ckanapi>=3.5',
         'flask>=1.0',
         'flask-caching',
