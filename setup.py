2cdba72c071ac2cf5851ee2d8b223a59cdfacc93
Dan Mihaila <danmihaila@gmail.com>
2020-04-27 15:04:58 +0300

    Preparing for 1.19 release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     name = 'hxl-proxy',
     packages = ['hxl_proxy'],
     package_data={'hxl_proxy': ['*.sql']},
-    version = "1.18",
+    version = "1.19",
     description = 'Flask-based web proxy for HXL',
     author='David Megginson',
     author_email='contact@megginson.com',
