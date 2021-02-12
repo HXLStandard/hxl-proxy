7bc855d5a41bcf696161c4907c1ead48fc23a120
Serban Teodorescu <teodorescu.serban@gmail.com>
2021-02-12 20:10:38 +0200

    mercy for the docker users

--- run-server.py
+++ run-server.py
@@ -2,5 +2,6 @@
 
 """Run a local dev copy of the HXL Proxy"""
 import sys
+import os
 from hxl_proxy import app
-app.run(debug=True, host='localhost')
+app.run(debug=True, host=os.getenv('FLASK_HOST', 'localhost'))
