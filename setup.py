47aea8bd996efd1a11872775876158e68df1c819
David Megginson <david.megginson@gmail.com>
2021-04-23 10:13:36 -0400

    Prepare for release 1.24 #346

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     zip_safe = False,
     install_requires=[
         'requests>=2.11',
-        'libhxl==4.23',
+        'libhxl==4.24',
         'ckanapi>=3.5',
         'flask>=1.0',
         'flask-caching',
