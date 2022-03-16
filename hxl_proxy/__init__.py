2db7e1fc15c4e4321a9b726b476590c43d02c1c0
Dan Mihaila <danmihaila@gmail.com>
2022-03-16 09:55:04 +0200

    Update __init__.py

--- hxl_proxy/__init__.py
+++ hxl_proxy/__init__.py
@@ -16,7 +16,7 @@ from flask_caching import Cache
 import werkzeug.datastructures
 from . import reverse_proxied
 
-__version__="1.24.2"
+__version__="1.24.3"
 """Module version number
 See https://www.python.org/dev/peps/pep-0396/
 """
