18a59085c802c6623f0b9f25330a17ecaccf09c7
David Megginson <david.megginson@gmail.com>
2022-07-28 09:26:02 -0400

    Make logging level configurable Default to "ERROR" if not specified Fixes #395

--- hxl_proxy/__init__.py
+++ hxl_proxy/__init__.py
@@ -14,32 +14,6 @@ See https://www.python.org/dev/peps/pep-0396/
 
 """
 
-
-#
-# Set up logging before any other imports, to get correct format in server logs
-#
-import logging, logging.config
-
-logging.config.dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(name)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
-
-logger = logging.getLogger(__name__)
-""" Python logger for this module """
-
-
 #
 # Continue with other imports
 #
@@ -60,13 +34,38 @@ app.wsgi_app = reverse_proxied.ReverseProxied(app.wsgi_app)
 
 
 #
-# General onfig
+# General config
 #
 app.config.from_object('hxl_proxy.default_config')
 if os.environ.get('HXL_PROXY_CONFIG'):
     app.config.from_envvar('HXL_PROXY_CONFIG')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+
+
+#
+# Set up logging before any other imports, to get correct format in server logs
+#
+import logging, logging.config
+
+logging.config.dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(name)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': app.config.get('LOGGING_LEVEL', 'ERROR'),
+        'handlers': ['wsgi']
+    }
+})
+
+logger = logging.getLogger(__name__)
+""" Python logger for this module """
 
 
 #
